54706277cda061227c7b6209f59c9c8f627cf3d7
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-31 17:40:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
     borderRadius: {
       'md': '18px',
     },
+    screens: {
+      'sm': '300px',
+      // => @media (min-width:525px) { ... }
+
+      'md': '600px',
+      // => @media (min-width: 850px) { ... }
+
+      'lg': '800px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1050px',
+      // => @media (min-width: 1200px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
