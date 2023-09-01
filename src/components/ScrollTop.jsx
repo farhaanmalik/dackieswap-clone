b01f147293d27832cf5d3452aa3d83f9e9c8dc0e
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-09-01 16:08:08 +0530

    Add files via upload

---
+++ src/components/ScrollTop.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+
+const ScrollTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const goTopBtn = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    };
+
+    const listenToScroll = () => {
+        let heightToHidden = 250;
+
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+        if (winScroll > heightToHidden) {
+            setIsVisible(true);
+        }
+        else {
+            setIsVisible(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", listenToScroll);
+        return () => window.removeEventListener("scroll", listenToScroll);
+    })
+
+    return (
+        <>
+            {isVisible && (
+                <div className="fixed bottom-28 md:right-20 sm:right-6 text-white px-4 py-2.5 bg-[#06f] cursor-pointer hover:opacity-70" style={{borderRadius:"15px"}} onClick={goTopBtn}>
+                    <i className="fa-solid fa-arrow-up"></i>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default ScrollTop
