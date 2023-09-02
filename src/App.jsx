cb65fcb814f01520177307bca01ae4199cd26310
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-09-02 09:17:24 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import ScrollTop from './components/ScrollTop'
 import Home from './pages/Home'
+import { useState } from "react"
 
 function App() {
 
+  const [darkTheme, setDarkTheme] = useState(false)
+
   return (
     <>
-      <Header />
-      <BottomNav />
-      <Home />
-      <Footer />
-      <ScrollTop />
+      <div className={darkTheme ? 'dark' : ''}>
+        <Header setDarkTheme={setDarkTheme} darkTheme={darkTheme}/>
+        <BottomNav />
+        <Home />
+        <Footer />
+        <ScrollTop />
+      </div>
     </>
   )
 }
